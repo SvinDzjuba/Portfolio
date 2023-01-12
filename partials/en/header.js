e4312e4804913d362e8605b69e54b4bc785f83dd
Maksim Dzjubenko <90320393+SvinDzjuba@users.noreply.github.com>
2023-01-12 23:19:35 +0200

    Remade the structure, making a course dropdown.

--- partials/en/header.js
+++ partials/en/header.js
@@ -5,10 +5,13 @@ const header = `
         <h2>Maksim Dzjubenko</h2>
     </a>
     <ul class="courses_list">
-        <li><a href="#">First course</a></li>
-        <li><a href="#">Second course</a></li>
-        <li><a href="#">Third course</a></li>
+        <li><a id="firstCourse" href="#">First course</a></li>
+        <li><a id="secondCourse" href="#">Second course</a></li>
+        <li><a id="thirdCourse" href="#">Third course</a></li>
     </ul>
+    <div class="dropdown_1">
+        <a href="prog_basics">Basics of programming</a>
+    </div>
     <ul class="languages">
         <li><a class="active" href="/">ENG</a></li>
         <li><a href="/et">EST</a></li>
